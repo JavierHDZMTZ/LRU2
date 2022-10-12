727923dfc66bdb1d54c3a45a58a50f2d72f45348
JavierHDZMTZ <kingstondarnel@gmail.com>
2022-10-11 21:38:40 -0500

    Proyecto SO2 prueba 1

--- src/lru2/Algoritmo_LRU2.java
+++ src/lru2/Algoritmo_LRU2.java
@@ -1,4 +1,17 @@
 package lru2;
 public class Algoritmo_LRU2 {
+    private int Matriz[][];
+    private int Tabla [][];
+    
+    private void Rellenar_Matriz(){
+    
+    }
+    private void Rellenar_FilasYColumnas(){
+    
+    }
+    
+    private void Fallo_de_Pagina(){
+    
+    }
     
 }
