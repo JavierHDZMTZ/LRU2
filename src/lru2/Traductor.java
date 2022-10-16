226061468517b9af1d6db1a3dec3ef8b859e323c
maqui <maqui@DESKTOP-G433P3L>
2022-10-16 14:48:43 -0500

    Traductor hecho

--- src/lru2/Traductor.java
+++ src/lru2/Traductor.java
@@ -1,4 +1,28 @@
 package lru2;
+
 public class Traductor {
-    //System.out.println("Hola mundo\n"); 
+    
+    public String dec_bin(int dec){
+        int modulo;
+        String bin = "";
+        
+        
+        while (dec > 0) {
+            modulo = (dec%2);
+            bin = modulo + bin;
+            dec = dec/2;
+        }
+        return bin;
+    }
+    
+    public int bin_dec(String bin) {
+        int dec=0;
+        
+        for (int i = 0; i < bin.length(); i++){
+            if (bin.charAt(i) == '1'){
+                dec += Math.pow(2, bin.length()-1-i); //Potencia
+            }
+        }
+        return dec;
+    }
 }
